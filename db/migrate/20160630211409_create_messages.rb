9a59f7e30456e3ef0176ca8e6450e2b11b3d3502
kh-fanis <kh-fanis@rambler.ru>
2016-07-01 14:38:47 +0300

    foreign key deleted from messages

--- db/migrate/20160630211409_create_messages.rb
+++ db/migrate/20160630211409_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-      t.references :sender,  index: true, foreign_key: true
-      t.references :reciver, index: true, foreign_key: true
+      t.references :sender,  index: true
+      t.references :reciver, index: true
       t.string     :content
       t.boolean    :deleted_by_sender
       t.boolean    :deleted_by_reciver
